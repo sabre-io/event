b76adbcb7e37dda7ed75acc7d39b1f3cfd9c4975
Evert Pot <evert@rooftopsolutions.nl>
2013-06-30 16:14:53 +0100

    year

--- lib/Sabre/Event/EventEmitter.php
+++ lib/Sabre/Event/EventEmitter.php
@@ -7,7 +7,7 @@ namespace Sabre\Event;
  *
  * Instantiate this class, or subclass it for easily creating event emitters. 
  * 
- * @copyright Copyright (C) 2007-2013 fruux GmbH. All rights reserved.
+ * @copyright Copyright (C) 2013 fruux GmbH. All rights reserved.
  * @author Evert Pot (http://evertpot.com/) 
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
