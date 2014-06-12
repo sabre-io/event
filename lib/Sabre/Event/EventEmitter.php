4d2327d4b21972751808f2432ce134a7f8919d5f
Evert Pot <me@evertpot.com>
2014-06-12 12:32:59 -0400

    Updated copyrights and link to license

--- lib/Sabre/Event/EventEmitter.php
+++ lib/Sabre/Event/EventEmitter.php
@@ -5,11 +5,11 @@ namespace Sabre\Event;
 /**
  * EventEmitter object.
  *
- * Instantiate this class, or subclass it for easily creating event emitters. 
- * 
- * @copyright Copyright (C) 2013 fruux GmbH. All rights reserved.
- * @author Evert Pot (http://evertpot.com/) 
- * @license https://raw.github.com/fruux/sabre-event/master/LICENSE
+ * Instantiate this class, or subclass it for easily creating event emitters.
+ *
+ * @copyright Copyright (C) 2013-2014 fruux GmbH. All rights reserved.
+ * @author Evert Pot (http://evertpot.com/)
+ * @license http://sabre.io/license/
  */
 class EventEmitter implements EventEmitterInterface {
 
--- lib/Sabre/Event/Version.php
+++ lib/Sabre/Event/Version.php
@@ -5,15 +5,15 @@ namespace Sabre\Event;
 /**
  * This class contains the version number for this package.
  *
- * @copyright Copyright (C) 2013 fruux GmbH (https://fruux.com/).
+ * @copyright Copyright (C) 2013-2014 fruux GmbH. All rights reserved.
  * @author Evert Pot (http://evertpot.com/)
- * @license https://raw.github.com/fruux/sabre-event/master/LICENSE
+ * @license http://sabre.io/license/
  */
 class Version {
 
     /**
      * Full version number
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
 }
