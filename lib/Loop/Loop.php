39c3648a2e9de3260dd865c051c57f56fd0a8a91
Stephan Unverwerth <s.unverwerth@gmx.de>
2016-09-19 20:13:55 +0200

    Fixed stream_select timeout calculation

--- lib/Loop/Loop.php
+++ lib/Loop/Loop.php
@@ -302,7 +302,7 @@ class Loop {
             $read = $this->readStreams;
             $write = $this->writeStreams;
             $except = null;
-            if (stream_select($read, $write, $except, $timeout ? 0 : null, $timeout ? $timeout : 0)) {
+            if (stream_select($read, $write, $except, ($timeout === null) ? null : 0, $timeout ? $timeout * 1000000 : 0)) {
 
                 // See PHP Bug https://bugs.php.net/bug.php?id=62452
                 // Fixed in PHP7
